893d3d157a1f9dcd714e8ae91ea9448d8aab2ec2
matsue-code <mbolamathew@icloud.com>
2025-09-14 10:40:47 +0300

    second commit

---
+++ main2.py
@@ -0,0 +1,11 @@
+class vehicle:
+    def __init__ (self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage= mileage
+audi=vehicle(240,19)
+print("audi max speed:",audi.max_speed)
+print("audi mileage:",audi.mileage)
+mercedes=vehicle(300,20)
+print("mercedes max speed:",mercedes.max_speed)
+print("mercedes mileage:",mercedes.mileage)
+    
